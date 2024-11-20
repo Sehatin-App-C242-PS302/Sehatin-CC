0ad2a220d398b8882d1a12fb849c917b19384060
yanuarioadimada <yanuarioadimada@gmail.com>
2024-11-20 23:25:49 +0700

    changes

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ const healthRoutes = require("./routes/healthRoutes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/health", healthRoutes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
