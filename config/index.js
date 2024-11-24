28e359b814658acb81539f3df5d2c92534483706
yanuarioadimada <yanuarioadimada@gmail.com>
2024-11-24 21:44:49 +0700

    changes

--- config/index.js
+++ config/index.js
@@ -1,19 +1,3 @@
-// require('dotenv').config();
-
-// module.exports = {
-//   db: {
-//     username: process.env.DB_USER || 'avnadmin',
-//     password: process.env.DB_PASSWORD || 'AVNS_rN_y8dcYawia_QxZKhP',
-//     database: process.env.DB_NAME || 'defaultdb',
-//     host: process.env.DB_HOST || 'http://mysql-sehatin-sehatin-cc.b.aivencloud.com/',
-//     port: process.env.DB_PORT || 14744,
-//     dialect: 'mysql',
-//   },
-//   server: {
-//     port: process.env.PORT || 14744,
-//   },
-// };
-
 require('dotenv').config();
 
 const config = {
@@ -37,6 +21,3 @@ const config = {
 };
 
 module.exports = config;
-
-
-
