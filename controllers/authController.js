649f302d68f5a70dba3b8728bafff3a774571c10
yanuarioadimada <yanuarioadimada@gmail.com>
2024-11-30 22:38:24 +0700

    auth controller changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,9 +6,17 @@ module.exports = {
     try {
       const { name, email, password } = req.body;
       const user = await registerUser(name, email, password);
-      res.status(201).json({ message: REGISTER_SUCCESS, user });
+      res.status(201).json({ 
+        success: true, 
+        message: REGISTER_SUCCESS, 
+        data: { user } 
+      });
     } catch (error) {
-      next(error);
+      res.status(400).json({ 
+        success: false, 
+        message: error.message 
+      });
+      next(error); // optional, jika ingin menggunakan middleware error handling
     }
   },
 
@@ -16,9 +24,43 @@ module.exports = {
     try {
       const { email, password } = req.body;
       const { user, token } = await loginUser(email, password);
-      res.status(200).json({ message: LOGIN_SUCCESS, token, user });
+      res.status(200).json({ 
+        success: true, 
+        message: LOGIN_SUCCESS, 
+        data: { token, user } 
+      });
     } catch (error) {
-      next(error);
+      res.status(401).json({ 
+        success: false, 
+        message: error.message 
+      });
+      next(error); // optional, jika ingin menggunakan middleware error handling
     }
   },
 };
+
+
+// const { registerUser, loginUser } = require('../services/authService');
+// const { REGISTER_SUCCESS, LOGIN_SUCCESS } = require('../constants/messages');
+
+// module.exports = {
+//   async register(req, res, next) {
+//     try {
+//       const { name, email, password } = req.body;
+//       const user = await registerUser(name, email, password);
+//       res.status(201).json({ message: REGISTER_SUCCESS, user });
+//     } catch (error) {
+//       next(error);
+//     }
+//   },
+
+//   async login(req, res, next) {
+//     try {
+//       const { email, password } = req.body;
+//       const { user, token } = await loginUser(email, password);
+//       res.status(200).json({ message: LOGIN_SUCCESS, token, user });
+//     } catch (error) {
+//       next(error);
+//     }
+//   },
+// };
