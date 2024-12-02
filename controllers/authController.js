899e34441d595130fdc711fdb4150311b099a00a
yanuarioadimada <yanuarioadimada@gmail.com>
2024-12-02 20:54:48 +0700

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,14 +9,14 @@ module.exports = {
       res.status(201).json({ 
         success: true, 
         message: REGISTER_SUCCESS, 
-        data: { user } 
+        user // langsung menyertakan user di bawah message
       });
     } catch (error) {
       res.status(400).json({ 
         success: false, 
         message: error.message 
       });
-      next(error); // optional, jika ingin menggunakan middleware error handling
+      next(error); // optional
     }
   },
 
@@ -27,40 +27,17 @@ module.exports = {
       res.status(200).json({ 
         success: true, 
         message: LOGIN_SUCCESS, 
-        data: { token, user } 
+        user: {
+          ...user, // tetap membungkus user dalam properti user
+          token,  // tambahkan token dalam objek user
+        }
       });
     } catch (error) {
       res.status(401).json({ 
         success: false, 
         message: error.message 
       });
-      next(error); // optional, jika ingin menggunakan middleware error handling
+      next(error); // optional
     }
   },
 };
-
-
-// const { registerUser, loginUser } = require('../services/authService');
-// const { REGISTER_SUCCESS, LOGIN_SUCCESS } = require('../constants/messages');
-
-// module.exports = {
-//   async register(req, res, next) {
-//     try {
-//       const { name, email, password } = req.body;
-//       const user = await registerUser(name, email, password);
-//       res.status(201).json({ message: REGISTER_SUCCESS, user });
-//     } catch (error) {
-//       next(error);
-//     }
-//   },
-
-//   async login(req, res, next) {
-//     try {
-//       const { email, password } = req.body;
-//       const { user, token } = await loginUser(email, password);
-//       res.status(200).json({ message: LOGIN_SUCCESS, token, user });
-//     } catch (error) {
-//       next(error);
-//     }
-//   },
-// };
